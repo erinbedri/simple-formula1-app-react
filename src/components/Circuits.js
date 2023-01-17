8db62b54e6400b3b5e5e23aa1e177f367ea9ac54
erinbedri <erinbedri@gmail.com>
2023-01-17 15:17:09 +0100

    seasoins seletor for circuits

--- src/components/Circuits.js
+++ src/components/Circuits.js
@@ -1,16 +1,26 @@
 import React, { useEffect, useState } from "react";
 
 import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 import Circuit from "./Circuit";
 
 export default function Circuits() {
-  const baseURL = "http://ergast.com/api/f1/2022/circuits.json";
-
   const [circuits, setCircuits] = useState([]);
   const [season, setSeason] = useState("2022");
+  const baseURL = `http://ergast.com/api/f1/${season}/circuits.json`;
+
+  const seasons = [];
+
+  for (var i = 2022; i >= 1950; i--) {
+    seasons.push(i);
+  }
+
+  const changeHandler = (e) => {
+    setSeason((oldState) => e.target.value);
+  };
 
   useEffect(() => {
     fetch(baseURL)
@@ -19,13 +29,24 @@ export default function Circuits() {
         setCircuits(result.MRData.CircuitTable.Circuits);
         setSeason(result.MRData.CircuitTable.season);
       });
-  }, []);
+  }, [season]);
 
   return (
     <>
       <Container className="mt-3 mb-3">
-        <h2>Circuits</h2>
-        <h5>Season {season}</h5>
+        <Row>
+          <h2>Circuits</h2>
+          <Form.Label>
+            <h5>Season {season}</h5>
+          </Form.Label>
+          <Col sm="3">
+            <Form.Select name="year" value={season} onChange={changeHandler}>
+              {seasons.map((year) => (
+                <option>{year}</option>
+              ))}
+            </Form.Select>
+          </Col>
+        </Row>
         <Row>
           {circuits.map((circuit) => (
             <Circuit key={circuit.circuitId} data={circuit} />
